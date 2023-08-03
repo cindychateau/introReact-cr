941fa3fd18b2de69b1d66d726e3bdccec89f72c1
Cynthia Castillo <ca.castillo@gmail.com>
2023-08-02 22:02:19 -0600

    Eventos y Estados

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
       <h1>¡Bienvenida a tu primer app en React!</h1>
       <MiPrimerComponente />
 
-      <PersonCard nombre="Juana" apellido="De Arco" edad="18" ciudad="Paris" />
-      <PersonCard nombre="Elena" apellido="De Troya" edad="30" ciudad="Roma" />
+      <PersonCard nombre="Juana" apellido="De Arco" edad={18} ciudad="Paris" />
+      <PersonCard nombre="Elena" apellido="De Troya" edad={30} ciudad="Roma" />
 
 
     </div>
--- src/componentes/PersonCard.js
+++ src/componentes/PersonCard.js
@@ -2,6 +2,13 @@ import React from "react";
 
 class PersonCard extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            edoCivil: "Soltero"
+        }
+    }
+
     render() {
         /*
         this.props = {
@@ -13,13 +20,33 @@ class PersonCard extends React.Component {
         //apellido = this.props.apellido
         //nombre = "Elena", apellido = "De Troya", edad = 30, ciudad = "Roma"
         return(
-            <div className="col-3 card">
+            <div className="col-3 card mb-3">
                 <h2>{nombre} {apellido}</h2>
                 <p>Edad: {edad}</p>
                 <p>Ciudad: {ciudad}</p>
+                <p>Estado Civil: {this.state.edoCivil}</p>
+                <button className="btn btn-success" onClick={ this.hizoClick } >Haz Click Aquí</button>
+                <input type="text" placeholder="Input" className="form-control" onChange={ ()=>alert("Cambió") } />
+                <button className="btn btn-warning" onClick={ this.cambiarEdoCivil }>Cambiar Edo. Civil</button>
             </div>
         )
 
+    }
+
+    hizoClick = () => {
+        console.log("El usuario hizo click");
+        alert("¡Gracias por hacer click!");
+    }
+
+    cambiarEdoCivil = () => {
+
+        if(this.state.edoCivil === "Soltero") {
+            this.setState({edoCivil: "Casado"});
+        } else {
+            this.setState({edoCivil: "Soltero"});
+        }
+ 
+        
     }
 
 }
